3143abcf5704a6b1a58b4d83ee26d50fd9bfdf2c
VanshulB <vanshulbhatia@gmail.com>
2022-07-23 22:39:17 +0530

    database updated

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,11 @@ const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
-    const post = orm.em.create(Post_1.Post, { id: 211, createdAt: null, updatedAt: null, title: "hello world" });
-    await orm.em.persistAndFlush(post);
-    console.log("------------------sql2--------------------");
-    await orm.em.nativeInsert(Post_1.Post, { createdAt: null, updatedAt: null, id: 321, title: "vanshul bhatia" });
+    await orm.getMigrator().up();
+    const posts = await orm.em.find(Post_1.Post, {});
+    console.log(posts);
 };
-main();
-console.log("Hello World");
+main().catch((err => {
+    console.log(err);
+}));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
-    const post = orm.em.create(Post, {id: 211, createdAt: null, updatedAt: null, title: "hello world"});
-    await orm.em.persistAndFlush(post);
-    console.log("------------------sql2--------------------");
-    await orm.em.nativeInsert(Post, {createdAt: null, updatedAt: null, id: 321, title: "vanshul bhatia"});
-};
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, {title: "hello world"});
+    // await orm.em.persistAndFlush(post);
 
- main();
- 
- console.log("Hello World");
- 
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+    
+};
+main().catch((err => {
+    console.log(err);
+}))
 
