d68319c49722414d4602ff5addcf748db83f8929
VanshulB <vanshulbhatia@gmail.com>
2022-07-28 00:52:36 +0530

    DAY 3 ☑️

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,33 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { validate } from "graphql";
+import { HelloResolvers } from "./resolvers/hello";
 
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
-    await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: "hello world"});
-    // await orm.em.persistAndFlush(post);
-
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    await orm.getMigrator().up(); 
     
-};
-main().catch((err => {
-    console.log(err);
-}))
+    const app = express();
 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolvers],
+            validate: false,
+        })
+    });
+    await apolloServer.start();
+    
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("Server started at port 4000");
+
+    }); 
+}; 
+main().catch((err => {
+     console.log(err); 
+    }));
